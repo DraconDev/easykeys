111164a2f25d44ee0b235cc4c6a62070249ba36f
DraconDev <dracsharp@gmail.com>
2024-01-26 06:36:27 +0000

    add basic

--- extension.js
+++
@@ -1,3 +0,0 @@
-vscode.commands.registerCommand("extension.moveCaretUpUsingDefault", () => {
-    vscode.commands.executeCommand("cursorUp"); // Trigger the default command
-});
---
+++ src/extension.js
@@ -0,0 +1,6 @@
+// import * as vscode from "vscode";
+
+// vscode.commands.registerCommand("extension.moveCaretUpUsingDefault", () => {
+//     vscode.commands.executeCommand("editor.action.moveLinesUpAction");
+// });
+// s;
